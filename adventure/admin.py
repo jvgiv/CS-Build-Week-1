b968cc626209fbb5a6e63c73706799fbe77b2263
jvgiv <jvgiv12@gmail.com>
2019-08-28 20:34:49 -0400

    Checking in

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Room, Player
 
-# Register your models here.
+# # Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "n_to", "e_to", "w_to", "s_to")
+
+admin.site.register(Room, RoomAdmin)
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("user", "currentRoom", "uuid")
+
+admin.site.register(Player, PlayerAdmin)
